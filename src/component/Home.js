ffd597c45714b04140542b257a892d5b7b18a403
monmasson <monicamasson@gmail.com>
2022-05-16 12:56:51 -0400

    commit

---
+++ src/component/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import About from "./About";
+const Home = () =>{
+const add = ( num1, num2) => {
+    return num1 + num2}
+
+
+ return(
+        <div>
+            Per Scholas is awesome! 
+
+add is equal to {add (2,3)}
+
+<About/>
+
+        </div>
+    )
+}
+export default Home
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Home from './component/Home';
+import About from './component/About';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Home/> 
   </React.StrictMode>
 );
 
